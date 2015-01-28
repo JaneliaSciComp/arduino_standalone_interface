d399ad730cd0e2621bf973534c0d28266a58660e
Peter Polidoro <peterpolidoro@gmail.com>
2015-01-28 17:08:10 -0500

    Preliminary code changes.

--- StandaloneInterface.cpp
+++ StandaloneInterface.cpp
@@ -1,164 +1,33 @@
 // ----------------------------------------------------------------------------
-// ModularDevice.cpp
+// StandaloneInterface.cpp
 //
 //
 // Authors:
 // Peter Polidoro polidorop@janelia.hhmi.org
 // ----------------------------------------------------------------------------
-#include "ModularDevice.h"
+#include "StandaloneInterface.h"
 
 
-using namespace ArduinoJson;
-
-namespace ModularDevice
+StandaloneInterface::StandaloneInterface(HardwareSerial &serial,
+                                         int enc_a_pin,
+                                         int enc_b_pin,
+                                         int enc_btn_pin,
+                                         int btn_pin)
 {
-
-FLASH_STRING(get_memory_free_method_name,"getMemoryFree");
-FLASH_STRING(reset_defaults_method_name,"resetDefaults");
-FLASH_STRING(set_serial_number_method_name,"setSerialNumber");
-FLASH_STRING(serial_number_parameter_name,"serial_number");
-
-ModularDevice::ModularDevice(HardwareSerial &serial) :
-  server_(serial)
-{
-  Method& get_memory_free_method = createMethod(get_memory_free_method_name);
-  get_memory_free_method.attachCallback(getMemoryFreeCallback);
-
-  Method& reset_defaults_method = createMethod(reset_defaults_method_name);
-  reset_defaults_method.attachCallback(resetDefaultsCallback);
-
-  Method& set_serial_number_method = createMethod(set_serial_number_method_name);
-  set_serial_number_method.attachCallback(setSerialNumberCallback);
-  Parameter& serial_number_parameter = modular_device.createParameter(serial_number_parameter_name);
-  serial_number_parameter.setRange(SERIAL_NUMBER_MIN,SERIAL_NUMBER_MAX);
-  set_serial_number_method.addParameter(serial_number_parameter);
+  display_ = NewhavenDisplay(serial);
+  encoder_ = Encoder(enc_b_pin,enc_a_pin);
+  enc_btn_pin_ = enc_btn_pin;
+  btn_pin_ = btn_pin;
 }
 
-void ModularDevice::setServerSerial(HardwareSerial &serial)
+void StandaloneInterface::setServerSerial(HardwareSerial &serial)
 {
   server_.setSerial(serial);
 }
 
-void ModularDevice::setName(const _FLASH_STRING &device_name)
+void StandaloneInterface::setName(const _FLASH_STRING &interface_name)
 {
-  server_.setName(device_name);
+  server_.setName(interface_name);
 }
 
-void ModularDevice::setModelNumber(const unsigned int model_number)
-{
-  server_.setModelNumber(model_number);
-}
-
-void ModularDevice::setFirmwareNumber(const unsigned int firmware_number)
-{
-  server_.setFirmwareNumber(firmware_number);
-}
-
-Method& ModularDevice::createMethod(const _FLASH_STRING &method_name)
-{
-  return server_.createMethod(method_name);
-}
-
-Method& ModularDevice::copyMethod(Method &method,const _FLASH_STRING &method_name)
-{
-  return server_.copyMethod(method,method_name);
-}
-
-Parameter& ModularDevice::createParameter(const _FLASH_STRING &parameter_name)
-{
-  return server_.createParameter(parameter_name);
-}
-
-Parameter& ModularDevice::copyParameter(Parameter &parameter,const _FLASH_STRING &parameter_name)
-{
-  return server_.copyParameter(parameter,parameter_name);
-}
-
-Parser::JsonValue ModularDevice::getParameterValue(const _FLASH_STRING &parameter_name)
-{
-  return server_.getParameterValue(parameter_name);
-}
-
-void ModularDevice::addNullToResponse(const char *key)
-{
-  server_.addNullToResponse(key);
-}
-
-void ModularDevice::addNullToResponse()
-{
-  server_.addNullToResponse();
-}
-
-void ModularDevice::addBooleanToResponse(const char *key, const boolean value)
-{
-  server_.addBooleanToResponse(key,value);
-}
-
-void ModularDevice::addBooleanToResponse(const boolean value)
-{
-  server_.addBooleanToResponse(value);
-}
-
-void ModularDevice::addKeyToResponse(const char *key)
-{
-  server_.addKeyToResponse(key);
-}
-
-void ModularDevice::startResponseObject()
-{
-  server_.startResponseObject();
-}
-
-void ModularDevice::stopResponseObject()
-{
-  server_.stopResponseObject();
-}
-
-void ModularDevice::startResponseArray()
-{
-  server_.startResponseArray();
-}
-
-void ModularDevice::stopResponseArray()
-{
-  server_.stopResponseArray();
-}
-
-void ModularDevice::startServer(const int baudrate)
-{
-  server_.startServer(baudrate);
-}
-
-void ModularDevice::handleServerRequests()
-{
-  server_.handleRequest();
-}
-
-void ModularDevice::resetDefaults()
-{
-  server_.resetDefaults();
-}
-
-void ModularDevice::setSerialNumber(const unsigned int serial_number)
-{
-  server_.setSerialNumber(serial_number);
-}
-}
-
-ModularDevice::ModularDevice modular_device(Serial);
-
-void ModularDevice::getMemoryFreeCallback()
-{
-  modular_device.addToResponse("memory_free", freeMemory());
-}
-
-void ModularDevice::resetDefaultsCallback()
-{
-  modular_device.resetDefaults();
-}
-
-void ModularDevice::setSerialNumberCallback()
-{
-  unsigned int serial_number = (long)modular_device.getParameterValue(serial_number_parameter_name);
-  modular_device.setSerialNumber(serial_number);
-}
+StandaloneInterface::StandaloneInterface standalone_interface(Serial);
--- StandaloneInterface.h
+++ StandaloneInterface.h
@@ -1,80 +1,31 @@
 // ----------------------------------------------------------------------------
-// ModularDevice.h
+// StandaloneInterface.h
 //
 //
 // Authors:
 // Peter Polidoro polidorop@janelia.hhmi.org
 // ----------------------------------------------------------------------------
-#ifndef MODULAR_DEVICE_H
-#define MODULAR_DEVICE_H
-#include "utility/Server.h"
+#ifndef STANDALONE_INTERFACE_H
+#define STANDALONE_INTERFACE_H
+#include "NewhavenDisplay.h"
+#include "Encoder.h"
 
 
-namespace ModularDevice
-{
-class Method;
-void getMemoryFreeCallback();
-void resetDefaultsCallback();
-void setSerialNumberCallback();
-
-class ModularDevice
+class StandaloneInterface
 {
 public:
-  ModularDevice(HardwareSerial &serial);
-  void setServerSerial(HardwareSerial &serial);
-  void setName(const _FLASH_STRING &device_name);
-  void setModelNumber(const unsigned int model_number);
-  void setFirmwareNumber(const unsigned int firmware_number);
-  Method& createMethod(const _FLASH_STRING &method_name);
-  Method& copyMethod(Method &method,const _FLASH_STRING &method_name);
-  Parameter& createParameter(const _FLASH_STRING &parameter_name);
-  Parameter& copyParameter(Parameter &parameter,const _FLASH_STRING &parameter_name);
-  ArduinoJson::Parser::JsonValue getParameterValue(const _FLASH_STRING &parameter_name);
-  template<typename T>
-  void createSavedVariable(const _FLASH_STRING &saved_variable_name, const T &default_value)
-  {
-    server_.createSavedVariable(saved_variable_name,default_value);
-  }
-  template<typename T>
-  void setSavedVariableValue(const _FLASH_STRING &saved_variable_name, T value)
-  {
-    server_.setSavedVariableValue(saved_variable_name,value);
-  }
-  template<typename T>
-  void getSavedVariableValue(const _FLASH_STRING &saved_variable_name, T &value)
-  {
-    server_.getSavedVariableValue(saved_variable_name,value);
-  }
-  template<typename T>
-  void addToResponse(const char *key, T value)
-  {
-    server_.addToResponse(key,value);
-  }
-  template<typename T>
-  void addToResponse(T value)
-  {
-    server_.addToResponse(value);
-  }
-  void addNullToResponse(const char *key);
-  void addNullToResponse();
-  void addBooleanToResponse(const char *key, const boolean value);
-  void addBooleanToResponse(const boolean value);
-  void addKeyToResponse(const char *key);
-  void startResponseObject();
-  void stopResponseObject();
-  void startResponseArray();
-  void stopResponseArray();
-  void startServer(const int baudrate);
-  void handleServerRequests();
+  StandaloneInterface(HardwareSerial &serial,
+                      int enc_a_pin,
+                      int enc_b_pin,
+                      int enc_btn_pin,
+                      int btn_pin);
 private:
-  Server server_;
-  void resetDefaults();
-  void setSerialNumber(const unsigned int serial_number);
-  friend void resetDefaultsCallback();
-  friend void setSerialNumberCallback();
+  NewhavenDisplay display_;
+  Encoder encoder_;
+  int enc_btn_pin_;
+  int btn_pin_;
 };
-}
 
-extern ModularDevice::ModularDevice modular_device;
+extern StandaloneInterface::StandaloneInterface standalone_interface;
 
 #endif
