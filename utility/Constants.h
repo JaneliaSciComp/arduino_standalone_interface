b9cae6c531a66c28825a2693d9c7b9345117ae71
Peter Polidoro <peterpolidoro@gmail.com>
2015-05-26 14:28:16 -0400

    Increased DISPLAY_LABEL_COUNT from 20 to 24.

--- utility/Constants.h
+++ utility/Constants.h
@@ -13,7 +13,7 @@ namespace Standalone
 {
 namespace constants
 {
-enum{DISPLAY_LABEL_COUNT_MAX=20};
+enum{DISPLAY_LABEL_COUNT_MAX=24};
 enum{DISPLAY_VARIABLE_COUNT_MAX=20};
 enum{INTERACTIVE_VARIABLE_COUNT_MAX=20};
 enum{FRAMES_COUNT_MAX=16};
