fd57696ea5b9328afecbc38d33ec9caa867d3e1e
Peter Polidoro <peter@polidoro.io>
2021-09-17 10:58:54 -0400

    Remove spurious comments

--- examples/StandaloneModularDevice/Callbacks.cpp
+++ examples/StandaloneModularDevice/Callbacks.cpp
@@ -10,21 +10,6 @@
 
 namespace callbacks
 {
-// Callbacks must be non-blocking (avoid 'delay')
-//
-// modular_server.getParameterValue must be cast to either:
-// const char*
-// long
-// double
-// bool
-// ArduinoJson::JsonArray&
-// ArduinoJson::JsonObject&
-//
-// For more info read about ArduinoJson parsing https://github.com/janelia-arduino/ArduinoJson
-//
-// modular_server.getSavedVariableValue type must match the saved variable default type
-// modular_server.setSavedVariableValue type must match the saved variable default type
-
 ModularDevice::ModularServer& modular_server = controller.getModularServer();
 
 void executeStandaloneCallbackCallback()
